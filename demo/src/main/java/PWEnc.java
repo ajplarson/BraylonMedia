3d2b9fcbbafbc2c9e8a6c7d94b873371ecc80c7e
VincentSquillace <vincentsquillace@icloud.com>
2019-08-22 11:16:02 -0500

    jfsk

---
+++ demo/src/main/java/PWEnc.java
@@ -0,0 +1,22 @@
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vince
+ */
+public class PWEnc {
+    public static void main(String[] args) {
+        String clearTxtPw = "password";
+        // BCrypt
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashedPw = encoder.encode(clearTxtPw);
+        System.out.println(hashedPw);
+    }
+}
