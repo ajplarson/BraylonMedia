9f0ba1fb9689feda6dc535045c3ac15d0b6bc322
Jack Elder <jackelder18@gmail.com>
2019-08-22 13:36:26 -0500

    here vincent

---
+++ demo/src/main/java/com/crudApp20/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.crudApp20.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author jackelder
+ */
+@Controller
+public class LoginController {
+    
+    @GetMapping("/login")
+    public String displayLogin(){
+        return "login";
+    }
+    
+}
