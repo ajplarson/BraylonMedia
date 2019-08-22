70b3d937f1b588de2cda35bd863e7b81632afd6c
Jack Elder <jackelder18@gmail.com>
2019-08-22 14:00:27 -0500

    new query

--- demo/src/main/java/com/crudApp20/demo/data/OrderDao.java
+++ demo/src/main/java/com/crudApp20/demo/data/OrderDao.java
@@ -1,6 +1,7 @@
 package com.crudApp20.demo.data;
 
 import com.crudApp20.demo.entities.Order;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -8,5 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author jackelder
  */
 public interface OrderDao extends JpaRepository<Order, Integer> {
+    
+    public List<Order> findByStatus(String status);
     
 }
