09dd7144f9325b22bbb10fec0b571605124be122
Jack Elder <jackelder18@gmail.com>
2019-08-22 15:29:02 -0500

    customer query

--- BrailOnMedia/src/main/java/com/brailonmedia/data/CustomerDao.java
+++ BrailOnMedia/src/main/java/com/brailonmedia/data/CustomerDao.java
@@ -1,7 +1,9 @@
 package com.brailonmedia.data;
 
 import com.brailonmedia.entities.Customer;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,5 +12,9 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CustomerDao extends JpaRepository<Customer, Integer> {
+    
+    @Query(value = "SELECT c.*, s.userId FROM SalesVisit s JOIN Customer c ON s.customerId = c.customerId WHERE s.userId = ?1",
+            nativeQuery = true)
+    public List<Customer> findAllCustomersByUser(Integer userId);
     
 }
