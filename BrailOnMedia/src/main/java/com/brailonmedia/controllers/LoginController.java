b81a1e1e6e8d818a1c1bf5020206ad0480a5245c
VincentSquillace <vincentsquillace@icloud.com>
2019-08-23 11:30:02 -0500

    asd

--- BrailOnMedia/src/main/java/com/brailonmedia/controllers/LoginController.java
+++ BrailOnMedia/src/main/java/com/brailonmedia/controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.brailonmedia.controllers;
 
 import com.brailonmedia.service.UserManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -26,6 +27,10 @@ public class LoginController {
 
     @GetMapping("/")
     public String displayLanding() {
+        String currentUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if(currentUserName.equalsIgnoreCase("Andrew")){
+            return "orders";
+        }
         return "redirect:/salesHome";
     }
 
