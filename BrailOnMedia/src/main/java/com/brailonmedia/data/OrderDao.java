d4c528cdf9be441492f9c82a56c49b68e6d85258
Jack Elder <jackelder18@gmail.com>
2019-08-23 10:34:40 -0500

    queries

--- BrailOnMedia/src/main/java/com/brailonmedia/data/OrderDao.java
+++ BrailOnMedia/src/main/java/com/brailonmedia/data/OrderDao.java
@@ -1,6 +1,7 @@
 package com.brailonmedia.data;
 
 import com.brailonmedia.entities.Order;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,5 +15,11 @@ import org.springframework.stereotype.Repository;
 public interface OrderDao extends JpaRepository<Order, Integer> {
     @Query(value = "SELECT * from `Order` o where `status` like ?1",nativeQuery=true)
     public List<Order> findAllByStatus(String status);
+    
+    @Query(value = "SELECT * FROM `Order` o WHERE `status` LIKE ?1 AND userId = ?2", nativeQuery=true)
+    public List<Order> findAllByStatusAndUserId(String status, Integer userId);
+    
+    @Query(value = "SELECT * FROM `Order` o WHERE `status` LIKE \"completed\" AND fulfillmentDate > ?1")
+    public List<Order> findAllCompletedDateLimited(LocalDateTime dateCutoff);
     
 }
